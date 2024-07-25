0846ad59fceb2b632468fdd812db78881c26e7f5
Frode Nordahl <fnordahl@ubuntu.com>
2024-07-25 16:00:04 +0200

    controller: Move LB by dp helpers to lb module.
    In a subsequent patch we need these functions in the
    route-exchange module.
    
    Signed-off-by: Frode Nordahl <fnordahl@ubuntu.com>

--- controller/lb.h
+++ controller/lb.h
@@ -20,6 +20,7 @@
 #include "lib/lb.h"
 
 struct sbrec_load_balancer;
+struct sbrec_load_balancer_table;
 
 struct ovn_controller_lb {
     struct hmap_node hmap_node;
@@ -41,6 +42,15 @@ struct ovn_controller_lb {
                                               */
 };
 
+/* Stores the load balancers that are applied to the datapath 'dp'. */
+struct load_balancers_by_dp {
+    struct hmap_node node;
+    const struct sbrec_datapath_binding *dp;
+    const struct sbrec_load_balancer **dp_lbs;
+    size_t n_allocated_dp_lbs;
+    size_t n_dp_lbs;
+};
+
 struct ovn_controller_lb *ovn_controller_lb_create(
     const struct sbrec_load_balancer *,
     const struct smap *template_vars,
@@ -50,6 +60,15 @@ void ovn_controller_lbs_destroy(struct hmap *ovn_controller_lbs);
 struct ovn_controller_lb *ovn_controller_lb_find(
     const struct hmap *ovn_controller_lbs,
     const struct uuid *uuid);
+
+struct hmap *load_balancers_by_dp_init(
+    const struct hmap *local_datapaths,
+    const struct sbrec_load_balancer_table *lb_table);
+struct load_balancers_by_dp *load_balancers_by_dp_find(
+    struct hmap *lbs,
+    const struct sbrec_datapath_binding *dp);
+void load_balancers_by_dp_cleanup(struct hmap *lbs);
+
 
 #endif /* OVN_CONTROLLER_LB_H */
 
